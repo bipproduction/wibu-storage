f1686e5d03052e3bf548a48b072daa687a89a604
bipproduction <bip.production.js@gmail.com>
2024-10-25 01:47:25 +0800

    reset password

--- x_test/test_copy.ts
+++ x_test/test_copy.ts
@@ -1,4 +1,4 @@
-import { apis } from "@/lib/routes";
+import { apies } from "@/lib/routes";
 
 const envToken = process.env.TEST_TOKEN as string;
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiY20wd2p1NXMzMDAwMGdtcHBhaXlyYWF0NSIsIm5hbWUiOiJtMiIsImVtYWlsIjoibTIiLCJBcGlLZXkiOlt7ImlkIjoiY20wd2p1NXNpMDAwMmdtcHB4MmxzOHZzbyIsIm5hbWUiOiJkZWZhdWx0In1dfSwiaWF0IjoxNzI1OTc5ODYwLCJleHAiOjQ4ODE3Mzk4NjB9.6UydaHlTN2txojIXrWPUS8Orsc4yK4DdJjzvP3gMBMI";
@@ -6,7 +6,7 @@ const orgn = "http://localhost:5000";
 (async () => {
   const copy = await fetch(
     orgn +
-      apis["/api/files/copy/[from]/[to]"]({
+      apies["/api/files/copy/[from]/[to]"]({
         from: "cm0wka2oe000cgmppwqm2ll48",
         to: "cm0wjursk0006gmppiugqjnv8"
       }),
--- x_test/test_delete.ts
+++ x_test/test_delete.ts
@@ -1,4 +1,4 @@
-import { apis } from "@/lib/routes";
+import { apies } from "@/lib/routes";
 
 const token =
   "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiY20wd2p1NXMzMDAwMGdtcHBhaXlyYWF0NSIsIm5hbWUiOiJtMiIsImVtYWlsIjoibTIiLCJBcGlLZXkiOlt7ImlkIjoiY20wd2p1NXNpMDAwMmdtcHB4MmxzOHZzbyIsIm5hbWUiOiJkZWZhdWx0In1dfSwiaWF0IjoxNzI1OTc5ODYwLCJleHAiOjQ4ODE3Mzk4NjB9.6UydaHlTN2txojIXrWPUS8Orsc4yK4DdJjzvP3gMBMI";
@@ -6,7 +6,7 @@ const orgn = "http://localhost:5000";
 async function test() {
   const res = await fetch(
     orgn +
-      apis["/api/files/delete/[dirId]/[name]"]({
+      apies["/api/files/delete/[dirId]/[name]"]({
         dirId: "cm0wjursk0006gmppiugqjnv8",
         name: "download.jpg"
       }),
